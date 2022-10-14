eba91b7afacaa7ea6666cd6e818ceced418cacf0
Ozioma Adibe <dibsdadon@gmail.com>
2022-10-13 17:04:44 -0700

    commit 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
+#include <stdio.h>
 /**
- * Main - First Printf tryout
+ * main - Entry point
  *
  * Return: 0
  */
-#include <stdio.h>
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
