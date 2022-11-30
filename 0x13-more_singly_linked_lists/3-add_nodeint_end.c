e1539d81b0050ad8c97745a832254847c9f5af2a
Festus Adibe <dibsdadon@gmail.com>
2022-11-30 23:07:10 +0000

    test2

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -30,10 +30,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 	{
 		while (temp->next != NULL)
-		{
-			temp = temp->next;											
+		{																	     temp = temp->next;											
 		}
 	        temp->next = new;
 	}
 
-	return (*head)
+	return (*head);
+}
